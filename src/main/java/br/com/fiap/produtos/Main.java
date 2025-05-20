2c9b90ff93776da4d097fcee1b2bbdc60b00af3c
joaocastro2 <joaocastrocntt06@gmail.com>
2025-05-20 19:16:13 -0300

    Teste de funcionamento

--- src/main/java/br/com/fiap/produtos/Main.java
+++ src/main/java/br/com/fiap/produtos/Main.java
@@ -1,17 +1,28 @@
 package br.com.fiap.produtos;
 
+import br.com.fiap.produtos.model.Categoria;
+import br.com.fiap.produtos.model.Produtos;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Categoria eletronicos = new Categoria();
+        eletronicos.setId(1l).setNome("Eletrônicos");
+
+        Produtos produto = new Produtos();
+        produto.setId(1l)
+                .setNome("Celular alcatel")
+                .setDescricao("Aparelho celular modelo alcatel 256gb")
+                .setCategoria(eletronicos)
+                .setDataCadastro(LocalDateTime.now())
+                .setPreco(BigDecimal.valueOf(800));
+
+        System.out.println("Categoria: " + eletronicos);
+        System.out.println("Produtos: " + produto);
     }
 }
